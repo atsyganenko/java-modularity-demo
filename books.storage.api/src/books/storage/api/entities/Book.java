de1a4e4da38d68e5022a9c40798ec326aceea527
a.tsyganenko <a.tsyganenko@intive.com>
2018-05-30 17:45:21 +0200

    add run.sh

--- books.storage.api/src/books/storage/api/entities/Book.java
+++
@@ -1,7 +0,0 @@
-package books.storage.api.entities;
-
-public interface Book {
-  int getId();
-  String getTitle();
-  double getPrice();
-}
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 
-javac --module-source-path /Users/a.tsyganenko/Documents/Work/java-modularity-demo -d out $(find . -name '*.java') -m books.inmemory.storage.api
+javac --module-source-path /Users/a.tsyganenko/Documents/Work/java-modularity-demo -d out $(find . -name '*.java') -m books.storage.api,main,books.inmemory.storage
 
+java --module-path out -m main/main.bookapp.BookApp;
