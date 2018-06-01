55aff25175dc70ba52abde64007f429a4d7c057a
a.tsyganenko <a.tsyganenko@intive.com>
2018-06-01 18:36:23 +0200

    JavaModularity - Google Guice Initial

--- books.inmemory.storage/module-info.java
+++ books.inmemory.storage/module-info.java
@@ -4,5 +4,4 @@ module books.inmemory.storage {
     requires guice;
     exports books.inmemory.storage.service.guice;
     exports books.inmemory.storage.entities;
-    opens books.inmemory.storage.service;
 }
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-
 javac -cp 'lib/*' --module-source-path . --module-path mods -d out $(find . -name '*.java')
+
+# java -cp 'lib/*' --module-path out:mods -m main/main.bookapp.BookApp
 
 java -cp 'lib/*' --add-opens java.base/java.lang=guice --module-path out:mods -m main/main.bookapp.BookApp
