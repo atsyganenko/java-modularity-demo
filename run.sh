3d5bab82268b9ad977843632006381194170ef03
a.tsyganenko <a.tsyganenko@intive.com>
2018-06-04 10:32:14 +0200

    jlink demo

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+# jlink --module-path out/:$JAVA_HOME/jmods --add-modules main --output myjre
+# du -h myjre/bin
 
 javac --module-source-path . -d out $(find . -name '*.java') -m books.storage.api,main,books.inmemory.storage
 
